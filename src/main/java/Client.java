2f8f61678c6373af6f9e357c30300f01eb6288b3
fisherek15 <fisherek15@gmail.com>
2020-11-09 22:15:04 +0100

    Small corrections connected with threads

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,42 +4,41 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Objects;
 
-public class Client implements Runnable{
+public class Client{
 
     public static void main(String[] args) {
-        Client client = new Client();
-        client.run();
-    }
 
-    @Override
-    public void run() {
+        Runnable task = () -> {
+            try {
+                Socket socket = new Socket("192.168.0.2", 7778);
+                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedReader brk = new BufferedReader(new InputStreamReader(System.in));
 
-        try {
-            Socket socket = new Socket("192.168.0.2", 7778);
-            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedReader brk = new BufferedReader(new InputStreamReader(System.in));
-
-            for(;;){
-                if(br.ready()){
-                    System.out.println(br.readLine());
-                }
-                if(brk.ready()){
-                    String dataToSend = brk.readLine();
-                    if (Objects.equals(dataToSend.toUpperCase(), Command.EXIT.toString())) {
-                        dos.writeBytes(Command.EXIT.toString());
-                        break;
-                    } else {
-                        dos.writeBytes(dataToSend + "\n");
+                for(;;){
+                    if(br.ready()){
+                        System.out.println(br.readLine());
+                    }
+                    if(brk.ready()){
+                        String dataToSend = brk.readLine();
+                        if (Objects.equals(dataToSend.toUpperCase(), Command.EXIT.toString())) {
+                            dos.writeBytes(Command.EXIT.toString());
+                            break;
+                        } else {
+                            dos.writeBytes(dataToSend + "\n");
+                        }
                     }
                 }
+
+                dos.close();
+                br.close();
+                brk.close();
+                socket.close();
+            } catch(Exception e){
+                e.printStackTrace();
             }
-            dos.close();
-            br.close();
-            brk.close();
-            socket.close();
-        } catch(Exception e){
-            e.printStackTrace();
-        }
+        };
+
+        new Thread(task).start();
     }
 }
--- src/main/java/ServerMulti.java
+++ src/main/java/ServerMulti.java
@@ -7,39 +7,28 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class ServerMulti implements Runnable{
+public class ServerMulti {
 
-    private ServerSocket serverSocket;
-    private ExecutorService pool;
-    private List<Handler> clientHandlers;
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
-        ServerMulti serverMulti = new ServerMulti(7778, 10);
-        serverMulti.run();
-    }
+        ServerSocket serverSocket = new ServerSocket(7778);
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+        List<Handler> clientHandlers = new ArrayList<>();
 
-    public ServerMulti(int port, int poolSize){
-        try{
-            this.serverSocket = new ServerSocket(port);
-            this.pool = Executors.newFixedThreadPool(poolSize);
-            this.clientHandlers = new ArrayList<>();
-        } catch(IOException ioException) {
-            ioException.printStackTrace();
-        }
-    }
-
-    @Override
-    public void run() {
-        try {
-            for (; ; ) {
-                Socket socket = serverSocket.accept();
-                Handler handler = new Handler(socket, clientHandlers);
-                pool.execute(handler);
-                clientHandlers.add(handler);
+        Runnable task = () -> {
+            try {
+                for (; ; ) {
+                    Socket socket = serverSocket.accept();
+                    Handler handler = new Handler(socket, clientHandlers);
+                    pool.execute(handler);
+                    clientHandlers.add(handler);
+                }
+            } catch (IOException ex){
+                pool.shutdown();
             }
-        } catch (IOException ex){
-            pool.shutdown();
-        }
+        };
+
+        new Thread(task).start();
     }
 
     void shutdownAndAwaitTermination(ExecutorService pool) {
